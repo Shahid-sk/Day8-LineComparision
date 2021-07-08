16d30a6bcaf198b305187eca5406b1cfbd01747d
Shahid-sk <mail2sk.shahid@gmail.com>
2021-07-08 16:02:03 +0530

    length of two end points have been calculated

--- src/linecomparision/LineComparision.java
+++ src/linecomparision/LineComparision.java
@@ -1,8 +1,22 @@
 package linecomparision;
 
+import java.util.Scanner;
+
 public class LineComparision {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparision Computation program");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter co-ordinate x1 : ");
+		double x1 = sc.nextDouble();
+		System.out.println("Enter co-ordinate y1 : ");
+		double y1 = sc.nextDouble();
+		System.out.println("Enter co-ordinate x2 : ");
+		double x2 = sc.nextDouble();
+		System.out.println("Enter co-ordinate y2 : ");
+		double y2 = sc.nextDouble();
+
+		double Lenght = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		System.out.println("Length of Line : " + Lenght);
 	}
 }
